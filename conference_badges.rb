f38b1a92206a39883b261c4d0b5da623b2a7c308
Evan Ratzlaff <ratzlaff92@gmail.com>
2019-06-15 20:22:34 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,14 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+def batch_badge_creator(array)
+  nuarray = []
+  array.each do |name|
+    nuarray.push("Hello, my name is #{name}.")
+  end
+  nuarray
+end
+def assign_rooms()
+  
+
+end
